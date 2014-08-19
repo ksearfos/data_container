b3e3de1a308660d0507604da69b4b88b3ac56cff
ksearfos <ksearfos@covermymeds.com>
2014-08-19 11:58:29 -0400

    making progress, have get and set and initialize and a good inspect method

--- lib/data_container.rb
+++ lib/data_container.rb
@@ -1,42 +1,29 @@
-class DataContainer
-  extend Enumerable
-
-  @ivars = []
-
-  class << self
-    if @ivars.is_a? Array then attr_accessor *@ivars
-    elsif @ivars.is_a? Hash then attr_accessor *@ivars.keys
-    else raise "Unable to create SettingsFairy class from a #{@ivars.class}"
-    end
-
-    def merge(other_settings, ignore_unknown = false)
-      if ignore_unknown   # merge in ALL ivars/values in other_settings
-        other_settings.each do |ivar, value|
-          next if value.nil?
-          self.set ivar, value
-        end
-      else     # merge in only those ivars/values that self shares
-        self.each do |ivar, value|
-          other_value = other_settings.get(ivar)
-          next if other_value.nil?
-          self.set ivar, other_value
-        end
-      end
-    end
-
-    def each(&block)
-      @ivars.each do |ivar|
-        value = get(ivar)
-        block.arity? == 2 ? block.call(ivar, value) : block.call(value)
-      end
-    end
-
-    def get(ivar)
-      instance_variable_get("@#{ivar}")
-    end
-
-    def set(ivar, value)
-      instance_variable_set("@#{ivar}", value)
-    end
+class DataContainer < Struct
+  def self.new(*args)
+    new_class = super
+    @instance = new_class.new
+    p "created instance: #{@instance}"
+    @instance
   end
+
+  def get(ivar)
+    send(ivar)
+  end
+
+  def set(ivar, value)
+    send("#{ivar}=", value)
+  end
+
+  def to_s
+    str = "#<DataContainer"
+    each_pair { |ivar, value| str << " #{ivar}=#{value.inspect}" }
+    str << ">"
+    str
+  end
+
+  def inspect
+    to_s
+  end
+
+  alias :data :members
 end
--- spec/data_container_spec.rb
+++ spec/data_container_spec.rb
@@ -2,13 +2,36 @@ require 'spec_helper'
 
 describe DataContainer do
   before(:all) do
-    @child = class TestContainer < DataContainer
-      @data = [:apple, :banana]
-      class << self; attr_reader :data; end
+    TestContainer = DataContainer.new(:apple, :banana)
+  end
+
+  describe "#initialize" do
+    it "creates instance variables for each of the array entries" do
+      expect(TestContainer.data).to eq([:apple, :banana])
+    end
+
+    it "sets each of those instance variables to nil" do
+      expect(TestContainer.apple).to be_nil
+      expect(TestContainer.banana).to be_nil
     end
   end
 
-  it "contains a list of data" do
-    expect(@child.data).to eq([:apple, :banana])
+  describe "#get" do
+    before(:all) do
+      TestContainer.apple = 'yum!'
+      TestContainer.banana = 'ewwww :('
+    end
+
+    it "retrieves the value of the variable name given" do
+      p TestContainer
+      expect(TestContainer.get(:apple)).to eq('yum!')
+    end
+  end
+
+  describe "#set" do
+    it "sets the variable given to the value provided" do
+      TestContainer.set(:banana, 'good with ice cream')
+      expect(TestContainer.banana).to eq('good with ice cream')
+    end
   end
 end
